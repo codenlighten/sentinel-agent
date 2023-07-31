14a3cdcf7e1aad6348a3ba06c6639debba8c6ff1
Greg Ward <61051193+codenlighten@users.noreply.github.com>
2023-07-31 15:13:29 -0400

    Update sentinel.sh

--- sentinel.sh
+++ sentinel.sh
@@ -1,10 +1,12 @@
-sudo apt-get update &&
-sudo apt-get install -y curl gzip tar fswatch jq &&
-curl -LO https://github.com/codenlighten/sentinel-agent/raw/main/sentinel-node-agent-installation.tgz &&
-tar xzf sentinel-node-agent-installation.tgz &&
-cd sentinel-node-agent-installer &&
-sudo bash install.sh &&
-sudo systemctl daemon-reload &&
-sudo systemctl enable sentinel-node-agent &&
-sudo systemctl start sentinel-node-agent &&
+#!/bin/bash
+set -e
+sudo apt-get update
+sudo apt-get install -y curl gzip tar fswatch jq
+curl -LO https://github.com/codenlighten/sentinel-agent/raw/main/sentinel-node-agent-installation.tgz
+tar xzf sentinel-node-agent-installation.tgz
+cd sentinel-node-agent-installer
+sudo bash install.sh
+sudo systemctl daemon-reload
+sudo systemctl enable sentinel-node-agent
+sudo systemctl start sentinel-node-agent
 sudo systemctl status sentinel-node-agent
